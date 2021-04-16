d16b3b29856f9f083a04d653dfadb02fe034cce7
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-04-16 14:01:19 +1200

    Update speed.js

--- publicstatic/Measure car speed and direction_files/speed.js
+++ publicstatic/Measure car speed and direction_files/speed.js
@@ -109,9 +109,9 @@ const speedlimit100 = document.getElementById("s100")
 const speedlimit30 = document.getElementById("s30")
 
 speedlimit100.addEventListener('click',()=>currentSpeedLimit=100)
-speedlimit80.addEventListener('click',()=>currentSpeedLimit=80)
-speedlimit50.addEventListener('click',()=>currentSpeedLimit=50)
-speedlimit30.addEventListener('click',()=>currentSpeedLimit=30)
+speedlimit80.addEventListener('click',()=>currentSpeedLimit=82)
+speedlimit50.addEventListener('click',()=>currentSpeedLimit=52)
+speedlimit30.addEventListener('click',()=>currentSpeedLimit=32)
 
 
 setInterval(soundAlarm,500)
