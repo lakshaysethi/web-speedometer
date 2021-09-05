5a00709a2160039c39d24f8bd79f922874c3536e
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-09-05 23:53:38 +0000

    removed caching

--- publicstatic/service-worker.js
+++ publicstatic/service-worker.js
@@ -1,18 +1,18 @@
 // give your cache a name
 const cacheName = 'my-cache';
-
+const version = 3
 // put the static assets and routes you want to cache here
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/404.html',
-  '/poli1.mp3',
-  '/Measure car speed and direction_files/speed.js',
-  '/Measure car speed and direction_files/style.css',
-  '/Measure car speed and direction_files/jquery-1.7.2.js.download',
-  '/Measure car speed and direction_files/d3.js.download',
+// const filesToCache = [
+//   '/',
+//   '/index.html',
+//   '/404.html',
+//   '/poli1.mp3',
+//   '/Measure car speed and direction_files/speed.js',
+//   '/Measure car speed and direction_files/style.css',
+//   '/Measure car speed and direction_files/jquery-1.7.2.js.download',
+//   '/Measure car speed and direction_files/d3.js.download',
 
-];
+// ];
 
 // the event handler for the activate event
 self.addEventListener('activate', e => self.clients.claim());
@@ -20,10 +20,11 @@ self.addEventListener('activate', e => self.clients.claim());
 // the event handler for the install event 
 // typically used to cache assets
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll(filesToCache))
-  );
+  console.log('caching removed on 6-sep 21')
+  // e.waitUntil(
+  //   caches.open(cacheName)
+  //   .then(cache => cache.addAll(filesToCache))
+  // );
 });
 
 // the fetch event handler, to intercept requests and serve all 
